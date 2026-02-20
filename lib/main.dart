709c023ccd88e749cc3fe9d5b7619b392b8a61da
rayanaa0005-sudo <rayanaa0005@gmail.com>
2026-02-21 03:09:18 +0600

    Raya1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Our First App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+        ),
+        body: Center(
+          child: Text(
+            'Welcome to Lifedrop!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
